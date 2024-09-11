2c7284f3a88c095aed0fceaa50dbf2fdf2e01497
Raul Posgrado <you@example.com>
2024-09-11 13:49:07 -0600

    ejercicio semana 6

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ Material de curso de posgrado de experimentación y métodos estadísticos
 - Semana 1: Introducción sobre el curso de "Experimentación y Métodos Estadísticos".
 - Semana 2: Creación de repositorio de GitHub y conexión con Estudio.
 - Semana 3: (próxima semana)
-- Semana 4: Explicación de las pruebas de estadísticas como normalidad, en la que destacan kolmogoro, shapiro y wilkings y explicación de hipótesis nula y alternativa. 
+- Semana 4: Explicación de las pruebas de estadísticas como normalidad, en la que destacan kolmogoro, Shapiro y wilkings y explicación de hipótesis nula y alternativa. 
 - Semana 5: Elaboración de laboratorio N°3 y tarea N°1.
+- Semana 6: (Próxima semana)
---
+++ Tareas/Anova.R
@@ -0,0 +1,33 @@
+
+# Clase Anova -------------------------------------------------------------
+
+sitios <- read.csv("Datos_Rascon_Anova.csv", header = T)
+
+tapply(sitios$DAP, sitios$Paraje, mean)
+tapply(sitios$EDAD, sitios$Paraje, mean)
+
+boxplot(sitios$DAP ~ sitios$Paraje)
+
+tapply(sitios$EDAD, sitios$Paraje, mean)
+
+shapiro.test(sitios$DAP)
+bartlett.test(sitios$DAP ~ sitios$Paraje)
+
+# los datos de DAF no son normales, aunque sus varianzas son homogeneas.
+
+sitios$dap_t <- sqrt(sitios$DAP)
+
+shapiro.test(sitios$dap_t)
+
+boxplot(sitios$dap_t ~ sitios$Paraje)
+
+mean(sitios$dap_t^2)
+mean(sitios$DAP)
+
+
+sit.aov <- aov(sitios$dap_t ~ sitios$Paraje)
+summary(sit.aov)
+
+TukeyHSD(sit.aov)
+
+plot(TukeyHSD(sit.aov))
